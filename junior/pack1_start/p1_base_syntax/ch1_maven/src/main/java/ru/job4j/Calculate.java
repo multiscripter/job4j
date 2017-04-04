9bb6f240beb5b23cd5f234a35a8a45efc183d89e
Gureev Ilya <ill-jah@yandex.ru>
2017-04-05 00:22:51 +0300

    task 4087. junior.pack1.p1.ch1.task1.3

--- junior/pack1_start/p1_base_syntax/ch1_maven/src/main/java/ru/job4j/Calculate.java
+++ junior/pack1_start/p1_base_syntax/ch1_maven/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+ * Class Calculate для вывода на консоль фразы "Hello, World!".
+ * @author Goureev Ilya
+ * @since 2017.04.04
+ * @version 1
+ */
 public class Calculate {
+    /**
+     * Главный метод.
+     * @param args массив параметров запуска.
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
---
+++ junior/pack1_start/p1_base_syntax/ch1_maven/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Goureev Ilya (mailto:ill-jah@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
