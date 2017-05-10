d0bbcab1940ed946ee12b936e708b4582ec7c552
webmaster <gi@redrocketmedia.ru>
2017-05-10 20:38:03 +0300

    task 10035. junior.pack1.p5.ch1.task1.3 v2

--- junior/pack1_start/p5_collections/ch1_collections/src/test/java/ru/job4j/convertlist/ConvertListTest.java
+++ junior/pack1_start/p5_collections/ch1_collections/src/test/java/ru/job4j/convertlist/ConvertListTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.convertlist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -18,11 +19,7 @@ public class ConvertListTest {
      */
     @Test
     public void testToList() {
-        int[] vals = new int[]{1, 2, 3, 4, 5, 6, 7, 0, 0};
-        List<Integer> expected = new ArrayList<>();
-        for (int a = 0; a < vals.length; a++) {
-            expected.add(vals[a]);
-        }
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 0, 0));
         int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
         ConvertList cl = new ConvertList();
         List result = cl.toList(array);
