896bfcd505aff5da33ed7686af06b73b02ef7100
Gureev Ilya <ill-jah@yandex.ru>
2017-05-10 23:07:04 +0300

    task 10035. junior.pack1.p5.ch1.task1.3 v4

--- junior/pack1_start/p5_collections/ch1_collections/src/main/java/ru/job4j/convertlist/ConvertList.java
+++ junior/pack1_start/p5_collections/ch1_collections/src/main/java/ru/job4j/convertlist/ConvertList.java
@@ -1,14 +1,13 @@
 package ru.job4j.convertlist;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
  * Класс ConvertList реализует функционал конвертации двумерного массива в ArrayList и наоборот.
  *
  * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
- * @version 1
+ * @version 2
  * @since 2017-05-10
  */
 class ConvertList {
@@ -35,14 +34,12 @@ class ConvertList {
     public int[][] toArray(List<Integer> list, int rows) {
         int size = (int) Math.ceil((double) list.size() / rows);
         int[][] array = new int[size][rows];
-        Iterator<Integer> iter = list.iterator();
-        label:
-        for (int a = 0; a < size; a++) {
-            for (int b = 0; b < rows; b++) {
-                if (!iter.hasNext()) {
-                    break label;
-                }
-                array[a][b] = iter.next();
+        int a = 0, b = 0;
+        for (Integer item : list) {
+            array[a][b++] = item;
+            if (b == rows) {
+                b = 0;
+                a++;
             }
         }
         return array;
