de741f512b496fcc16d76fe4439f8c72cc2a3a43
Gureev Ilya <ill-jah@yandex.ru>
2017-05-13 01:54:15 +0300

    junior.pack1.p5.ch3.task3.1 v2

--- junior/pack1_start/p5_collections/ch3_sorting/src/main/java/ru/job4j/sorting/SortUser.java
+++ junior/pack1_start/p5_collections/ch3_sorting/src/main/java/ru/job4j/sorting/SortUser.java
@@ -1,6 +1,5 @@
 package ru.job4j.sorting;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 import java.util.Set;
@@ -8,7 +7,7 @@ import java.util.Set;
  * Class SortUser реализует сортировку пользователей.
  *
  * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
- * @version 1
+ * @version 2
  * @since 2017-05-12
  */
 class SortUser {
@@ -18,13 +17,6 @@ class SortUser {
      * @return TreeSet с отсортированными пользователями.
      */
     Set<User> sort(List<User> list) {
-        list.sort(new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return Integer.compare(o1.getAge(), o2.getAge());
-            }
-        });
-        TreeSet<User> set = new TreeSet<>(list);
-        return set;
+        return new TreeSet<>(list);
     }
 }
--- junior/pack1_start/p5_collections/ch3_sorting/src/test/java/ru/job4j/sorting/SortUserTest.java
+++ junior/pack1_start/p5_collections/ch3_sorting/src/test/java/ru/job4j/sorting/SortUserTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.sorting;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.TreeSet;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -8,7 +9,7 @@ import static org.junit.Assert.assertEquals;
  * Class SortUserTest тестирует класс SortUser.
  *
  * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
- * @version 1
+ * @version 2
  * @since 2017-05-12
  */
 public class SortUserTest {
@@ -20,13 +21,20 @@ public class SortUserTest {
         String[] names = new String[]{"Emelyan", "Frol", "Nikifor", "Dormidont", "Alampy", "Nafanail"};
         int[] ages = new int[]{73, 67, 76, 88, 63, 79};
         ArrayList<User> usersList = new ArrayList<>();
-        TreeSet<User> expected = new TreeSet<>();
+        ArrayList<User> expected = new ArrayList<>();
         for (int a = 0; a < names.length; a++) {
             usersList.add(new User(names[a], ages[a]));
             expected.add(new User(names[a], ages[a]));
         }
+        expected.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
         SortUser su = new SortUser();
-        TreeSet<User> result = (TreeSet) su.sort(usersList);
+        TreeSet<User> sorted = (TreeSet) su.sort(usersList);
+        ArrayList<User> result = new ArrayList<>(sorted);
         assertEquals(expected, result);
     }
 }
