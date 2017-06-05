c67790d8475c4223d9e3bee2f9fe258e129e476e
Gureev Ilya <ill-jah@yandex.ru>
2017-06-05 23:26:00 +0300

    #161 junior.pack2.p5.ch3.task4 v3 NodeCycleChecker. While instead recursion

--- junior/pack2_standard/p5_collections_pro/ch3_list/src/main/java/ru/job4j/list/NodeCycleChecker.java
+++ junior/pack2_standard/p5_collections_pro/ch3_list/src/main/java/ru/job4j/list/NodeCycleChecker.java
@@ -5,7 +5,7 @@ package ru.job4j.list;
  *
  * @param <T> параметризированный тип.
  * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
- * @version 2
+ * @version 3
  * @since 2017-05-31
  */
 class NodeCycleChecker<T> {
@@ -15,24 +15,25 @@ class NodeCycleChecker<T> {
     private int pos = 0;
     /**
      * Проверяет список узлов на ссылочную замкнутость.
-     * @param first первый элемент списка.
+     * @param x первый элемент списка.
      * @return true если список зациклен. Иначе false.
      */
-    public boolean hasCycle(Node<T> first) {
+    public boolean hasCycle(Node<T> x) {
         boolean result = false;
-        Node<T> x = first.getNext();
-        if (x != null) {
+        Node<T> x2 = x.getNext();
+        while (x != null || x2 != null) {
             this.pos++;
-            Node<T> x2 = x;
             for (int a = this.pos * 2; a > this.pos; a--) {
-                x2 = x2.getNext();
                 if (x2 == null) {
                     break;
                 }
+                x2 = x2.getNext();
             }
-            if (x2 != null) {
-                result = x == x2 ? true : this.hasCycle(x);
+            if (x == x2) {
+                result = true;
+                break;
             }
+            x = x.getNext();
         }
         return result;
     }
