d93e2ef43fce1a312ec1c2bcd4af54a871657627
Gureev Ilya <ill-jah@yandex.ru>
2017-06-08 01:00:06 +0300

    #999 junior.pack2.p5.ch5.task1 'Create model User'

---
+++ junior/pack2_standard/p5_collections_pro/ch5_map/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,136 @@
+package ru.job4j.map;
+
+import java.util.GregorianCalendar;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+/**
+ * Класс UserTest тестирует класс User.
+ *
+ * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
+ * @version 1
+ * @since 2017-06-07
+ */
+public class UserTest {
+    /**
+     * Объект пользователя.
+     */
+    private User user;
+    /**
+     * Действия перед тестом.
+     */
+    @Before
+    public void beforeTest() {
+        this.user = new User("Vasisualiy", new GregorianCalendar(1965, 11, 20), 1);
+    }
+    /**
+     * Тестирует compareTo(). Объекты равны.
+     */
+    @Test
+    public void testCompareTo() {
+        User expected = new User("Vasisualiy", new GregorianCalendar(1965, 11, 20), 1);
+        assertTrue(expected.compareTo(this.user) == 0);
+    }
+    /**
+     * Тестирует compareTo(). Левый меньше правого.
+     */
+    @Test
+    public void testCompareToLower() {
+        User expected = new User("Vasisualiy", new GregorianCalendar(1965, 11, 20), 0);
+        assertTrue(expected.compareTo(this.user) < 0);
+    }
+    /**
+     * Тестирует compareTo(). Левый больше правого.
+     */
+    @Test
+    public void testCompareToGreater() {
+        User expected = new User("Vasisualiy", new GregorianCalendar(1965, 11, 20), 2);
+        assertTrue(expected.compareTo(this.user) > 0);
+    }
+    /**
+     * Тестирует equals(). Объекты равны.
+     */
+    @Test
+    public void testEqualsTrue() {
+        User expected = new User("Vasisualiy", new GregorianCalendar(1965, 11, 20), 1);
+        assertTrue(expected.equals(this.user));
+    }
+    /**
+     * Тестирует equals(). Объекты не равны.
+     */
+    @Test
+    public void testEqualsFalse() {
+        User expected = new User("Vasisualiy", new GregorianCalendar(1965, 11, 20), 2);
+        assertFalse(expected.equals(this.user));
+    }
+    /**
+     * Тестирует getBirthday().
+     */
+    @Test
+    public void testGetBirthday() {
+        String expected = "1965-11-20";
+        String result = this.user.getBirthday();
+        assertEquals(expected, result);
+    }
+    /**
+     * Тестирует getBirthdayObj().
+     */
+    @Test
+    public void testGetBirthdayObj() {
+        GregorianCalendar expected = new GregorianCalendar(1965, 11, 20);
+        GregorianCalendar result = this.user.getBirthdayObj();
+        assertEquals(expected, result);
+    }
+    /**
+     * Тестирует getChildren().
+     */
+    @Test
+    public void testGetChildren() {
+        assertEquals(1, this.user.getChildren());
+    }
+    /**
+     * Тестирует getName().
+     */
+    @Test
+    public void testGetName() {
+        assertEquals("Vasisualiy", this.user.getName());
+    }
+    /**
+     * Тестирует setBirthday().
+     */
+    @Test
+    public void testSetBirthday() {
+        GregorianCalendar expected = new GregorianCalendar(1956, 03, 04);
+        GregorianCalendar newBirthday = new GregorianCalendar(1956, 03, 04);
+        this.user.setBirthday(newBirthday);
+        GregorianCalendar result = this.user.getBirthdayObj();
+        assertEquals(expected, result);
+    }
+    /**
+     * Тестирует setChildren().
+     */
+    @Test
+    public void testSetChildren() {
+        this.user.setChildren(2);
+        assertEquals(2, this.user.getChildren());
+    }
+    /**
+     * Тестирует setName().
+     */
+    @Test
+    public void testSetName() {
+        this.user.setName("Apolinariy");
+        assertEquals("Apolinariy", this.user.getName());
+    }
+    /**
+     * Тестирует setToString().
+     */
+    @Test
+    public void testToString() {
+        String expected = "User{name: Vasisualiy, birthday: 1965-11-20, children: 1}";
+        String result = this.user.toString();
+        assertEquals(expected, result);
+    }
+}
