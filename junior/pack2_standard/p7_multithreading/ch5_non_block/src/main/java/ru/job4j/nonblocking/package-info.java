5c80d8fd91d395c0fa91ca25f00db58ea760a1da
webmaster <gi@redrocketmedia.ru>
2017-08-04 12:52:50 +0300

    Added junior.pack2.p7.ch5

---
+++ junior/pack2_standard/p7_multithreading/ch5_non_block/src/main/java/ru/job4j/nonblocking/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for junior.pack2.p7.ch5. Non-blocking algoritm.
+*
+* @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 1
+* @since 2017-08-04
+*/
+package ru.job4j.nonblocking;
