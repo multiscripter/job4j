c3ed5eda61cbe1cfe4772c3563cef67d47f4caf6
webmaster <gi@redrocketmedia.ru>
2017-10-19 15:53:31 +0300

    Added junior.pack2.p9

---
+++ junior/pack2_standard/p9_web/ch1_struct/src/main/java/ru/job4j/struct/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for junior.pack2.p9.ch1. Structure of client-Server applications.
+*
+* @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 1
+* @since 2017-10-19
+*/
+package ru.job4j.jdbc;
