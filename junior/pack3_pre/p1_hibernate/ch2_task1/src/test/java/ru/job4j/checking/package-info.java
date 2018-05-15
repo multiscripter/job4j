48020758e0b884116d6157b3e93f09e82c920f4b
cyberbotx <ill-jah@yandex.ru>
2018-05-15 21:45:41 +0300

    #4744 junior.pack3.p1.ch2.task1 v2.0 33h 'Car storage using Hibernate'

---
+++ junior/pack3_pre/p1_hibernate/ch2_task1/src/test/java/ru/job4j/checking/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for junior.pack3.p1.ch2.task1. Car storage.
+ *
+ * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
+ * @version 2018-04-27
+ * @since 2018-04-27
+ */
+package ru.job4j.checking;
