3d8e547511af9c9f871cf3da7aa5618ed20ef391
cyberbotx <ill-jah@yandex.ru>
2018-05-23 12:37:26 +0300

    deleted ch2_task1

--- junior/pack3_pre/p1_hibernate/ch2_task1/src/main/java/ru/job4j/models/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for junior.pack3.p1.ch2.task1. Car storage.
-*
-* @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
-* @version 2018-04-26
-* @since 2018-04-26
-*/
-package ru.job4j.models;
--- junior/pack3_pre/p1_hibernate/ch2_task1/src/test/java/ru/job4j/checking/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package for junior.pack3.p1.ch2.task1. Car storage.
- *
- * @author Gureyev Ilya (mailto:ill-jah@yandex.ru)
- * @version 2018-04-27
- * @since 2018-04-27
- */
-package ru.job4j.checking;
