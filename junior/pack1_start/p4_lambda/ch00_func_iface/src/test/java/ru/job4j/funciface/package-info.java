2a4ab9e4697e2c4fba17afed50224a1ed228ab02
cyberbotx <ill-jah@yandex.ru>
2018-09-10 22:59:10 +0300

    #70608. junior.pack1.p4.ch1.task00. Function interfaces.

---
+++ junior/pack1_start/p4_lambda/ch00_func_iface/src/test/java/ru/job4j/funciface/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for junior.pack1.p4.ch0.task0 Функциональные итерфейсы.
+*
+* @author Goureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 2018-09-10
+* @since 2018-09-10
+*/
+package ru.job4j.funciface;
