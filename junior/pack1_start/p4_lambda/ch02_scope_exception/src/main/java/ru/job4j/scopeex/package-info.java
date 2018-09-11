2bb015d27f622c5b77d28549e53a992ec3fa9a55
cyberbotx <ill-jah@yandex.ru>
2018-09-12 00:04:54 +0300

    #70608. junior.pack1.p4.ch1.task01. References to methods. #70609. junior.pack1.p4.ch1.task02. lambda scopes and exceptions.

---
+++ junior/pack1_start/p4_lambda/ch02_scope_exception/src/main/java/ru/job4j/scopeex/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for junior.pack1.p4.ch0.task2 #70609 Зона видимости в лямбда и исключения.
+*
+* @author Goureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 2018-09-11
+* @since 2018-09-11
+*/
+package ru.job4j.scopeex;
