b401c1f35a36b298fbfed2ae381f180959c78277
cyberbotx <ill-jah@yandex.ru>
2018-09-13 22:46:26 +0300

    #24251. junior.pack1.p4.ch1.task1. 4h. Calculate function ranges.

---
+++ junior/pack1_start/p4_lambda/ch1_mathfunc/src/test/java/ru/job4j/mathfunc/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for junior.pack1.p4.ch1.task1 #24251 Подсчет функции в диапазоне.
+*
+* @author Goureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 2018-09-12
+* @since 2018-09-12
+*/
+package ru.job4j.mathfunc;
