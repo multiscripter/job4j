0089c8029a7d163de8397836484009aa51e066a4
cyberbotx <ill-jah@yandex.ru>
2018-11-28 21:44:19 +0300

    Added #20329 junior.pack1.p4.ch2 Stream API

---
+++ junior/pack1_trainee/p4_lambda/ch2_stream_api/src/test/java/ru/job4j/streamapi/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for junior.pack1.p4.ch2 Stream API.
+*
+* @author Goureyev Ilya (mailto:ill-jah@yandex.ru)
+* @version 2018-11-28
+* @since 2018-11-28
+*/
+package ru.job4j.streamapi;
