f5708b9a7b677d7000f4861cf0daf296850e1cd7
cyberbotx <ill-jah@yandex.ru>
2018-12-06 21:30:44 +0300

    #24259 junior.pack1.p4.ch2.task1. Added recode #35008. v2

--- junior/pack1_trainee/p4_lambda/ch2_stream_api/src/main/java/ru/job4j/comparator/StringComparator.java
+++ junior/pack1_trainee/p4_lambda/ch2_stream_api/src/main/java/ru/job4j/comparator/StringComparator.java
@@ -19,18 +19,18 @@ public class StringComparator implements Comparator<String> {
     public int compare(String left, String right) {
         int result = 0;
         int a = 0;
-        try {
-            for (; a < left.length(); a++) {
-                result = Character.compare(left.charAt(a), right.charAt(a));
-                if (result != 0) {
-                    break;
-                }
+        for (; a < left.length() && a < right.length(); a++) {
+            result = Character.compare(left.charAt(a), right.charAt(a));
+            if (result != 0) {
+                break;
             }
-        } catch (IndexOutOfBoundsException ex) {
-            result = 1;
         }
-        if (result == 0 && a < right.length()) {
-            result = -1;
+        if (result == 0) {
+            if (left.length() < right.length()) {
+                result = -1;
+            } else if (left.length() > right.length()) {
+                result = 1;
+            }
         }
         return result;
     }
